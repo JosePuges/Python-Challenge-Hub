4029da8d989d2d67d58095dad222770b08b327b4
josepuges <pascualpuges84@gmail.com>
2026-02-13 10:53:05 +0100

    Fear: actualizando mas ejercicios

---
+++ Ejercicios de Tipos de Datos Simples/Solucion-5.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pregunte al usuario por el número 
+# de horas trabajadas y el coste por hora. 
+# Después debe mostrar por pantalla la paga que le corresponde.
+
+horas = float(input("Horas trabajadas: "))
+coste = float(input("Coste por hora: "))
+paga = horas * coste
+
+print("La paga es: ", paga)
---
+++ Ejercicios de Tipos de Datos Simples/Solucion-6.py
@@ -0,0 +1,17 @@
+# Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros),
+# calcule el índice de masa corporal y lo almacene en una variable, 
+# y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> 
+# es el índice de masa corporal calculado redondeado con dos decimales.
+
+# Solicitud de datos
+peso = input("¿Cuál es tu peso en kg? ")
+estatura = input("¿Cuál es tu estatura en metros?")
+
+# Cálculo del IMC con redondeo a 2 decimales
+imc = round(float(peso)/float(estatura)**2,2)
+
+# Mostrar resultado
+print("Tu índice de masa corporal es " + str(imc))
+
+
+
