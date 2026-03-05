4b0fa5ccfcfd390e04dd6e22aeabf587a9d602e3
JosePuges <pascualpuges84@gmail.com>
2026-03-05 20:08:31 +0100

    Fear: Crenado juego adivina el numero

---
+++ Adivina el numero/Adivina-el-numero-funciones.py
@@ -0,0 +1,44 @@
+import random
+
+def pedir_numero():
+    try:
+        valor = int(input("Introdce un numero: "))
+        return valor
+    except ValueError:
+        print("ERROR, introduce un numero valido")
+
+def jugar_partida():
+    numero_secreto = random.randint(1, 100)
+    intentos = 0
+    print(" ¡ Nueva partida ! He pensado un numero del 1 al 100. ¿ Podras adivinarlo ?")
+    while True:
+        intento = pedir_numero()
+        intentos = +1
+        if intento < numero_secreto:
+            print(" El numero es mas alto")
+        elif intento > numero_secreto:
+            print(" El numero es mas bajo")
+        else:
+            print(f" Enhorabuena has acetado el numero en {intentos} intentos")
+            return intentos    
+        
+def main():
+    seguir_jugando = "s"
+    mejor_puntuacion = None
+
+    while seguir_jugando.lower() == "s":
+        resultado = jugar_partida()
+
+
+    if mejor_puntuacion is None or resultado < mejor_puntuacion:
+        mejor_puntuacion = resultado
+        print(f" ¡ Nuevo record : {mejor_puntuacion} intentos !")
+    else:
+        print(f" ¡ Tu mejor puntuacion sigue siendo: {mejor_puntuacion} intentos!")
+
+        seguir_jugando = input(" ¿ Quieres seguir jugando ?: s/n")
+
+    print(f" Fin del juego, tu mejor puntuaciona ha sido. {mejor_puntuacion} intentos")
+
+
+    
---
+++ Ejercicio-pares/pares.py
@@ -0,0 +1,10 @@
+# par o impar
+
+numero = int(input("Por favor ingresa tu numero: "))
+
+if numero:
+    numero %2 
+    print("el numero es par")
+else:
+
+    print("el numero es impar")
